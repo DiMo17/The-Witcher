7a179e399721e56f51ceb809df071d0873ac9f5d
DiMo17 <45009193+DiMo17@users.noreply.github.com>
2021-07-25 03:26:00 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    loop: true,
+    navigation: {
+        nextEl: '.arrow',
+      },
+    breakpoints: {
+    // when window width is >= 540px
+        540: {
+            slidesPerView: 2
+            }
+        },
+    });
+
+
+let menuButton = document.querySelector('.menu-button');
+let menu = document.querySelector('.header');
+
+menuButton.addEventListener('click', function() {
+    menuButton.classList.toggle('menu-button-active');
+    menu.classList.toggle('header-active');
+});
